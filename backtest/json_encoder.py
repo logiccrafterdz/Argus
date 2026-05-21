730cd4d001d988783737c96923238e76470e6e59
logiccrafterdz <logiccrafterdz@gmail.com>
2026-05-21 12:45:28 +0100

    Step 1: Add json_encoder.py - NumpyEncoder for JSON serialization

---
+++ backtest/json_encoder.py
@@ -0,0 +1,18 @@
+﻿import json
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer): return int(obj)
+        elif isinstance(obj, np.floating): return float(obj)
+        elif isinstance(obj, np.ndarray): return obj.tolist()
+        elif isinstance(obj, pd.Series): return obj.tolist()
+        elif isinstance(obj, datetime): return obj.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            try:
+                if pd.isna(obj): return None
+            except:
+                pass
+            return super(NumpyEncoder, self).default(obj)
